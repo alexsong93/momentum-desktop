2df1e2b177f5c915c3e334a492896f0f9ce5f355
Seokhyun Song <songs@us.ibm.com>
2017-01-11 20:52:41 -0500

    Implement basic flow for chaning desktop background

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 'use strict';
 
 var express = require('express');
+var momentum = require('../lib/momentum');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.send('it works!');
+router.get('/change', function(req, res) {
+  momentum.init()
+    .then(function(result) {
+      res.send(result);
+    })
+    .catch(function(err) {
+      throw new Error(err);
+    });
 });
 
 module.exports = router;
--- utils/logger.js
+++ utils/logger.js
@@ -2,14 +2,10 @@
 
 var bunyan = require('bunyan');
 var assign = require('lodash/assign');
-var nconf = require('./conf');
-var loggerConfig = nconf.get('logger');
 
 bunyan.getLogger = function(name, opts) {
   return bunyan.createLogger(assign({
-    name: name,
-    level: loggerConfig.level,
-    stream: process.stdout
+    name: name
   }, opts));
 };
 
